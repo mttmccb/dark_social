7aa4a7381330e3bfe01183d1dba7270761c25653
Matt McCabe <mattmadethis2@gmail.com>
2015-05-10 21:10:09 +0100

    Profile take data from adn api (tokenless)

--- src/profile.js
+++ src/profile.js
@@ -1,38 +1,41 @@
 import { computedFrom } from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
 
 export class Profile {
-  constructor(){
-    this.profileCount = [];
-    this.addProfileCount({ title: 'followers', icon: 'arrow-circle-left', count: 228 });
-    this.addProfileCount({ title: 'following', icon: 'arrow-circle-right', count: 199 });
-    this.addProfileCount({ title: 'posts', icon: 'pencil-square-o', count: 17000 });
-    console.log('hello');
-  } 
+  static inject() { return [HttpClient]; };
+  constructor(http) {
+    this.http = http;
+  }
   
-  profilePicture = 'profilePicture.png';
-  coverPhoto = 'coverPhoto.jpeg';
-  heading = 'You Profile';
-  displayName = 'Matt McCabe';
-  domainVerified = "mttmccb.net";
-  accountType = 'human'; // feed | bot
-  followerCount = 228;
-  followingCount = 199;
-  postCount = 17000;
-  userName = 'mttmccb';
-  bio = 'I\'m a Daddy, Husband, Contrarian, Engineer and a Web Developer. #teamappdiction #teamappminimal #team256 ';
-  //Getters can't be observed with Object.observe, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
-  //@computedFrom('firstName', 'lastName')
-  get fullName() {
-    return `${this.displayName} @${this.userName}`;
+  apiURL = 'https://api.app.net';
+  user_id = 59521;
+  heading = 'Your Profile';
+
+  activate() {
+      return this.http.get(this.apiURL + '/users/' + this.user_id + '/posts').then(get => {
+        this.response = JSON.parse(get.response);
+    });
   }
 
-  addProfileCount (count) {
-    this.profileCount.push(count);
-  }
-  get accountTypeIcon() {
-    switch (this.accountType) {
+  get displayName()     { return this.response.data[0].user.name; }
+  get userName()        { return this.response.data[0].user.name; }
+  get coverImageUrl()   { return this.response.data[0].user.cover_image.url; }
+  get avatarImageUrl()  { return this.response.data[0].user.avatar_image.url; }
+  get bio()             { return this.response.data[0].user.description.text }
+  get bioHtml()         { return this.response.data[0].user.description.html }
+  get verifiedLink()    { return this.response.data[0].user.verified_link; }
+  get verifiedDomain()  { return this.response.data[0].user.verified_domain; }
+  get userType()        { return this.response.data[0].user.type; } 
+  get followers()       { return this.response.data[0].user.counts.followers; }
+  get following()       { return this.response.data[0].user.counts.following; }
+  get posts()           { return this.response.data[0].user.counts.posts; }
+  get stars()           { return this.response.data[0].user.counts.stars; }
+  
+  @computedFrom('displayName', 'userName')
+  get fullName() { return `${this.displayName} @${this.userName}`; }
+
+  get userTypeIcon() {
+    switch (this.userType) {
       case 'human':
         return 'user';
 
@@ -54,3 +57,4 @@ export class UpperValueConverter {
     return value && value.toUpperCase();
   }
 }
+
