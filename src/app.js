7867c62dd7fd838f5a508af592f9894ec6e70c54
Matt McCabe <mattmadethis2@gmail.com>
2015-05-15 00:17:12 +0100

    Remove welcome, updated .gitignore

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ export class App {
   configureRouter(config, router){
     config.title = 'Dark.Social';
     config.map([
-      { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
-      { route: 'profile',  moduleId: './profile',      nav: true }
+      { route: ['','profile'],  moduleId: './profile',      nav: true }
     ]);
 
     this.router = router;
