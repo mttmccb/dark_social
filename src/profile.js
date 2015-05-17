37e52da5e1cc2146278c95764741bf36171c515a
Matt McCabe <mattmadethis2@gmail.com>
2015-05-18 00:01:23 +0100

    Moved some functions into utilty

--- src/profile.js
+++ src/profile.js
@@ -1,6 +1,7 @@
 import { computedFrom } from 'aurelia-framework';
 import { HttpClient } from 'aurelia-http-client';
 import { AdnAPI } from './adn-api';
+import { parseDate, functiontofindIndexByKeyValue } from './utility';
 
 export class Profile {
   static inject() { return [AdnAPI, HttpClient]; }
@@ -147,21 +148,6 @@ export class Profile {
   toggleVisible(e) {
     this.showBanner = !this.showBanner;
   }
-}
-
-function parseDate(dateToParse) {
-  return Math.round(( new Date() - Date.parse(dateToParse) ) /3600000);
-}
-
-function functiontofindIndexByKeyValue(arraytosearch, key, valuetosearch) {
- 
-  for (var i = 0; i < arraytosearch.length; i++) {
-   
-    if (arraytosearch[i][key] == valuetosearch) {
-      return i;
-    }
-  }
-  return -1;
 }
 
 export class UpperValueConverter {
--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,19 @@
 export function areEqual(obj1, obj2) {
 	return Object.keys(obj1).every((key) => obj2.hasOwnProperty(key) && (obj1[key] === obj2[key]));
 }
+
+
+export function parseDate(dateToParse) {
+  return Math.round(( new Date() - Date.parse(dateToParse) ) /3600000);
+}
+
+export function functiontofindIndexByKeyValue(arraytosearch, key, valuetosearch) {
+ 
+  for (var i = 0; i < arraytosearch.length; i++) {
+   
+    if (arraytosearch[i][key] == valuetosearch) {
+      return i;
+    }
+  }
+  return -1;
+}
