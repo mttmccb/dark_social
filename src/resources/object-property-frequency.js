de45223cc05930d7a802bfd31d482f58bff7ba7a
Matt McCabe <mattmadethis2@gmail.com>
2015-05-28 12:21:27 +0100

    Fixed client links to close #55

--- src/resources/object-property-frequency.js
+++ src/resources/object-property-frequency.js
@@ -1,15 +1,25 @@
 import { findIndexByKeyValue } from './utility';
 
 export class ObjectPropertyFrequencyValueConverter {
-  toView(array, object, property) {
+  toView(array, object, property,additionalProperty) {
     var mentionMap = [];
     if (array.length>0) {
-      mentionMap.push({name: array[0][object][property], count: 0});
+      var item = {name: array[0][object][property], count: 0};
+      if (additionalProperty) {
+        item[additionalProperty] = array[0][object][additionalProperty];
+      }
+      mentionMap.push(item);
       array.forEach((mention) => {
         var index = findIndexByKeyValue(mentionMap,'name',mention[object][property]);
-        index ===-1?
-          mentionMap.push({ name: mention[object][property], count: 1}) :
+        if (index ===-1) {
+          item = {name: mention[object][property], count: 1};
+          if (additionalProperty) {
+            item[additionalProperty] = mention[object][additionalProperty];
+          }          
+          mentionMap.push(item);
+        } else {          
           mentionMap[index].count++;        
+        }
       });
     }
     return mentionMap;
