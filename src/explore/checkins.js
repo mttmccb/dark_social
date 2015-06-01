7993e6d7717f7ab2f0f49e20eb58f0c01a3d3057
Matt McCabe <mattmadethis2@gmail.com>
2015-06-01 22:24:19 +0100

    Split out modal image into custom element

--- src/explore/checkins.js
+++ src/explore/checkins.js
@@ -2,32 +2,13 @@ import { inject, bindable } from 'aurelia-framework';
 import { AdnAPI } from '../adn-api';
 import imagesLoaded from 'imagesloaded';
 import Masonry from 'masonry-layout';
-import { EventAggregator } from 'aurelia-event-aggregator';
-import { ImageViewed } from '../resources/messages';
-import { ADNImage } from '../models/adn-image';
-import 'mapbox.js';
-import 'mapbox.js/theme/style.css!';
 
-@inject(AdnAPI, EventAggregator)
+@inject(AdnAPI)
 export class Checkins {
 
-  constructor(api, ea) {
+  constructor(api) {
     this.api = api;
     this.posts = [];
-    this.showing = false;
-    this.adnimage = new ADNImage();
-    this.ea = ea;
-    ea.subscribe(ImageViewed, msg => this.showModal(msg.image));
-  }
-
-  showModal(image) {
-    this.adnimage = image;
-    this.showing = true;
-  }
-
-  hideModal() {
-    this.showing = false;
-    this.adnimage = new ADNImage();
   }
 
   activate() {
@@ -38,38 +19,6 @@ export class Checkins {
 
   attached() {
     this.createMasonry();
-    L.mapbox.accessToken = 'pk.eyJ1IjoibXR0bWNjYiIsImEiOiJhNTZkOTMzYWYxMWQ1NzVkZTUxOWYwMTZkMWEyNWNhNiJ9.eOp4wDfZ_aYnPmK5Ul3axA';
-    var map = L.mapbox.map('map', 'mapbox.streets')
-      .setView([40, -74.50], 9);
-
-    var featureLayer = L.mapbox.featureLayer().addTo(map);
-    var geojson = {
-      type: 'FeatureCollection',
-      features: []
-    };
-
-    this.posts.forEach(function (post) {
-      post.annotations.forEach(function (annotation) {
-        if (annotation.type === "net.app.ohai.location"
-          || annotation.type === "net.app.core.checkin"
-          || annotation.type === "net.app.core.geolocation") {
-          if (annotation.value.longitude) {
-            geojson.features.push({
-              type: 'Feature',
-              geometry: {
-                type: 'Point',
-                coordinates: [
-                  annotation.value.longitude,
-                  annotation.value.latitude
-                ]
-              }
-            });
-          }
-        }
-      });
-    });
-    featureLayer.setGeoJSON(geojson);
-    map.fitBounds(featureLayer.getBounds());
   }
 
   createMasonry() {
--- src/explore/trending.js
+++ src/explore/trending.js
@@ -2,29 +2,13 @@ import { inject } from 'aurelia-framework';
 import { AdnAPI } from '../adn-api';
 import imagesLoaded from 'imagesloaded';
 import Masonry from 'masonry-layout';
-import { EventAggregator } from 'aurelia-event-aggregator';
-import { ImageViewed } from '../resources/messages';
-import { ADNImage } from '../models/adn-image';
 
-@inject(AdnAPI, EventAggregator)
+@inject(AdnAPI)
 export class Trending {
 
-  constructor(api, ea) {
+  constructor(api) {
     this.api = api;
     this.posts = [];
-    this.adnimage = new ADNImage();
-    this.ea = ea;
-    ea.subscribe(ImageViewed, msg => this.showModal(msg.image));
-  }
-
-  showModal(image) {
-    this.adnimage = image;
-    this.showing = true;
-  }
-
-  hideModal() {
-    this.showing = false;
-    this.adnimage = new ADNImage();
   }
 
   activate() {
