49540a73691bf610560dcf9b333c56cfb945925e
Matt McCabe <mattmadethis2@gmail.com>
2015-06-02 00:05:28 +0100

    Remove gulp tasks I'm not using an docs folder

--- build/paths.js
+++ build/paths.js
@@ -10,7 +10,6 @@ module.exports = {
   style: 'styles/**/*.css',
   output: outputRoot,
   sourceMapRelativePath: '../' + appRoot,
-  doc:'./doc',
   e2eSpecsSrc: 'test/e2e/src/*.js',
   e2eSpecsDist: 'test/e2e/dist/'
 };
