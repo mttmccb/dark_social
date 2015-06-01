077d65a5b72cf4d70a8ded5a1b3d18b956977172
Matt McCabe <mattmadethis2@gmail.com>
2015-06-01 23:00:12 +0100

    Stripped out image custom element from profile

--- src/profile.js
+++ src/profile.js
@@ -3,34 +3,17 @@ import { AdnAPI } from './adn-api';
 import { activationStrategy } from 'aurelia-router';
 import imagesLoaded from 'imagesloaded';
 import Masonry from 'masonry-layout';
-import { EventAggregator } from 'aurelia-event-aggregator';
-import { ImageViewed } from '../resources/messages';
-import { ADNImage } from '../models/adn-image';
 
-@inject(AdnAPI, EventAggregator)
+@inject(AdnAPI)
 export class Profile {
 
   determineActivationStrategy() {
     return activationStrategy.replace;
   }
 
-  constructor(api, ea) {
+  constructor(api) {
     this.api = api;
     this.posts = [];
-    this.showing = false;
-    this.adnimage = new ADNImage();
-    this.ea = ea;
-    ea.subscribe(ImageViewed, msg => this.showModal(msg.image));
-  }
-
-  showModal(image) {
-    this.adnimage = image;
-    this.showing = true;
-  }
-
-  hideModal() {
-    this.showing = false;
-    this.adnimage = new ADNImage();
   }
 
   activate(params, query, route) {
