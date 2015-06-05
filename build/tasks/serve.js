cb4af2c9fc258117e65a01c842ad2fb639e67488
Matt McCabe <mattmadethis2@gmail.com>
2015-06-05 21:37:27 +0100

    Updated serve task for pushstate

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,19 +1,33 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var fs = require("fs"),
+  path = require("path"),
+  url = require("url"),
+  gulp = require("gulp"),
+  browserSync = require("browser-sync");
 
-// this task utilizes the browsersync plugin
-// to create a dev server instance
-// at http://localhost:9000
-gulp.task('serve', ['build'], function(done) {
+// The default file if the file/path is not found
+var defaultFile = "index.html";
+
+// I had to resolve to the previous folder, because this task lives inside a ./tasks folder
+// If that's not your case, just use `__dirname`
+var folder = path.resolve(__dirname, "../../");
+
+gulp.task("serve", ['build'], function () {
+
   browserSync({
     open: false,
     port: 9000,
     server: {
-      baseDir: ['.'],
+      baseDir: '.',
       middleware: function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        next();
+        var fileName = url.parse(req.url);
+        fileName = fileName.href.split(fileName.search).join("");
+        var fileExists = fs.existsSync(folder + fileName);
+        if (!fileExists && fileName.indexOf("browser-sync-client") < 0) {
+          req.url = "/" + defaultFile;
+        }
+        return next();
       }
     }
-  }, done);
+  });
+
 });
