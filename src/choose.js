644a175b95f46ebc4cd799b6e17205cc91190599
Matt McCabe <mattmadethis2@gmail.com>
2015-06-21 14:56:58 +0100

    Update front page to remove chooser and set a header, tweaked nav spacing

--- src/choose.js
+++ src/choose.js
@@ -16,23 +16,4 @@ export class Choose {
 	openProfile(name) {
 		this.theRouter.navigateToRoute("userprofile", { user_id: name });
 	}
-
-	navigate(e) {
-		switch (e.target.value) {
-			case "profile/random":
-			case "trending":
-			case "photos":
-			case "conversations":
-			case "checkins":
-				this.theRouter.navigate(e.target.value);
-				break;
-
-			case "kebab":
-				window.location.href = "http://en.wikipedia.org/wiki/Kebab";
-				break;
-
-			default:
-				break;
-		}
-	}
 }
