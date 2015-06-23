5fb33d9f24baf9841bf50f181ef7b70c2263a21a
Matt McCabe <mattmadethis2@gmail.com>
2015-06-23 10:11:29 +0100

    Closed #143

--- src/templates/posts/post/post-back.js
+++ src/templates/posts/post/post-back.js
@@ -1,5 +1,15 @@
-import { bindable } from 'aurelia-framework';
+import { inject, bindable } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
+@inject(Router)
 export class PostBackCustomElement {
   @bindable post = null;
+
+  constructor(router) {
+    this.theRouter = router;
+  }
+
+  loadUserRoute(user) {
+    this.theRouter.navigateToRoute("userprofile", { user_id: user });
+  }
 }
