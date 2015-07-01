cabe2a93cc59b7266dd7cbe25da79daa97aedd91
Matt McCabe <mattmadethis2@gmail.com>
2015-07-01 22:14:52 +0100

    Closed #168

--- src/profile/followers.js
+++ src/profile/followers.js
@@ -2,28 +2,43 @@ import { inject } from 'aurelia-framework';
 import { AdnAPI } from 'services/adn-api';
 import { activationStrategy } from 'aurelia-router';
 import { State } from '../services/state';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { RefreshedView, LoadMore, ApiStatus } from 'resources/messages';
 
-@inject(AdnAPI, State)
+@inject(AdnAPI, State, EventAggregator)
 export class Followers {
-	
-  constructor(api, state) {
+
+  constructor(api, state, ea) {
     this.api = api;
     this.data = [];
     this.state = state;
+    this.ea = ea;
+    this.loadMore = ea.subscribe(LoadMore, msg => this.loadFollowers(this.user_id, true));
   }
 
   activate(params, query, route) {
     this.user_id = this.state.user_id;
-    return this.loadFollowers(params.user_id || this.user_id);
+    return this.loadFollowers(params.user_id || this.user_id, false);
   }
 
   refresh() {
-    return this.loadFollowers(this.user_id);
+    return this.loadFollowers(this.user_id, false);
   }
 
-  loadFollowers(user) {
-    return this.api.load('followers', { id: this.user_id, more: false }).then(data => {
-      this.data = data;
+	deactivate() {
+    this.loadMore();
+	}
+
+  loadFollowers(user, more) {
+    return this.api.load('followers', { id: user, more: more }).then(data => {
+
+      if (this.data.length > 0 && this.data[0].id === data[0].id) {
+        this.ea.publish(new ApiStatus(`No More...`, { status: 'info' }));
+      } else {
+        this.data = more ? this.data.concat(data) : data;
+      }
+    }).then(() => {
+      this.ea.publish(new RefreshedView());
     });
   }
 }
