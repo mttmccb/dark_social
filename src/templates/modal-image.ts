ef9c3044cccde934b38ef0a30fd4d55337cbfdf5
Matt McCabe <mattmadethis2@gmail.com>
2015-09-14 21:31:22 +0100

    Updated settings Fixed post path

--- src/templates/modal-image.ts
+++ src/templates/modal-image.ts
@@ -6,13 +6,18 @@ import { ADNImage } from '../models/adn-image';
 @autoinject
 export class ModalImageCustomElement {
   adnimage: ADNImage;
+  private imageViewed: any;
   
   constructor(private ea: EventAggregator, private showing : boolean) {
     this.showing = false;
     this.adnimage = new ADNImage();
-    ea.subscribe(ImageViewed, (msg: any) => this.showModal(msg.image));
+    this.imageViewed = ea.subscribe(ImageViewed, (msg: any) => this.showModal(msg.image));
   }
 
+  detached() {
+    this.imageViewed();
+  }
+  
   showModal(image: ADNImage) {
     this.showing = true;
     this.adnimage = image;
